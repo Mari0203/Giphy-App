3c5ec46a2e49f303e9ad6cb886d23c104f1f777f
Mari0203 <mari.brands@pm.me>
2019-06-16 22:52:57 -0500

    Updated response function to append gifs with ratings

--- app.js
+++ app.js
@@ -34,31 +34,54 @@ buttonGenerator();
   when that specific button is clicked. */
 $(".classForButton").on("click", function(e) {
   var emotion = $(this).attr("data-emotion");
-  alert($(this).html()); // check to make sure this function works.
-  });
+  var emotionSelected = $(this).text();
+
+  // My API Key:
+  var api_key = "lBtXwR7tqfrf553s7E99F3o2oAa4EC61";
+
+  // URL to be used for get gifs from Giphy with result output limit set to 10.
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    emotionSelected +
+    "&api_key=" +
+    api_key +
+    "&limit=10";
+
+  //Performing the AJAX GET request, then store an array of results in the variable:
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    // Function to execute when the data gets retrieved from the API:
+    .then(function(response) {
+      var results = response.data;
+      console.log(results);
+
+      // For Loop to be executed for each result item:
+      $("#gifs-display").html("");
+      for (var i = 0; i < results.length; i++) {
+        var image = $("<img>");
+        image.attr("data-still", results[i].images.original_still.url);
+        image.attr("data-animate", results[i].images.original.url);
+        image.css("margin-left", "3px");
+        image.attr("src", results[i].images.original_still.url);
+
+        $("#gifs-display").append(image);
+        var ratingInfo = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+        $("#gifs-display").append(ratingInfo);         
+      }
+    });
+});
 
 // Function to dynamically create a new button with the keyword submitted by the user;
-
 /* Do this by adding the new word into the emotion array, and then calling the buttonGenerator 
    function to dynamically create a new button. */
 $("#submission-box").on("submit", function(e) {
-  e.preventDefault();  // Prevents the form's submit's default action of refreshing the page.
+  e.preventDefault(); // Prevents the form's submit's default action of refreshing the page.
   emotion.push($("#entry").val());
   buttonGenerator();
 });
 
 // Check to see if the new word was added to the emotion array:
 console.log(emotion);
-
-
-// Store my API Key and URL to be used for get gifs from Giphy with result output limit set to 10.
-var api_key = lBtXwR7tqfrf553s7E99F3o2oAa4EC61;
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=" + api_key + "&limit=10";
-
-//Performing the AJAX GET request, then store an array of results in the variable:
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  var results = response.data;
-});
