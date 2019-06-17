e5a8319d962eeaac55adc13662c177a12733771a
Mari0203 <mari.brands@pm.me>
2019-06-16 23:11:40 -0500

    Added pause/play function to output gifs

--- app.js
+++ app.js
@@ -64,13 +64,32 @@ $(".classForButton").on("click", function(e) {
         var image = $("<img>");
         image.attr("data-still", results[i].images.original_still.url);
         image.attr("data-animate", results[i].images.original.url);
+        image.attr("data-state", "still");
         image.css("margin-left", "3px");
         image.attr("src", results[i].images.original_still.url);
+        image.addClass("gif");
 
         $("#gifs-display").append(image);
         var ratingInfo = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
         $("#gifs-display").append(ratingInfo);         
       }
+
+      // To animate and pause the result gif image when a user clicks
+      $(".gif").on("click", function() {
+
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
     });
 });
 
