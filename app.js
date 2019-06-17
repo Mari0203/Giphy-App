263bce397f41fd86a7351b90d40b609313d6aa24
Mari0203 <mari.brands@pm.me>
2019-06-16 23:27:10 -0500

    Debugged: Moved the event listener function to inside the buttonGenerator function

--- app.js
+++ app.js
@@ -25,73 +25,74 @@ var buttonGenerator = function() {
     button.text(emotion[i]);
     $("#buttonList").append(button);
   }
+
+  /* Event listener for all button elements and save info into each data-emotion 
+  when that specific button is clicked. */
+  $(".classForButton").on("click", function(e) {
+    var emotion = $(this).attr("data-emotion");
+    var emotionSelected = $(this).text();
+
+    // My API Key:
+    var api_key = "lBtXwR7tqfrf553s7E99F3o2oAa4EC61";
+
+    // URL to be used for get gifs from Giphy with result output limit set to 10.
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      emotionSelected +
+      "&api_key=" +
+      api_key +
+      "&limit=10";
+
+    //Performing the AJAX GET request, then store an array of results in the variable:
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+      // Function to execute when the data gets retrieved from the API:
+      .then(function(response) {
+        var results = response.data;
+        console.log(results);
+
+        // For Loop to be executed for each result item:
+        $("#gifs-display").html("");
+        for (var i = 0; i < results.length; i++) {
+          var image = $("<img>");
+          image.attr("data-still", results[i].images.original_still.url);
+          image.attr("data-animate", results[i].images.original.url);
+          image.attr("data-state", "still");
+          image.css("margin-left", "3px");
+          image.attr("src", results[i].images.original_still.url);
+          image.addClass("gif");
+
+          $("#gifs-display").append(image);
+          var ratingInfo = $("<p>").text(
+            "Rating: " + results[i].rating.toUpperCase()
+          );
+          $("#gifs-display").append(ratingInfo);
+        }
+
+        // To animate and pause the result gif image when a user clicks
+        $(".gif").on("click", function() {
+          // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+          var state = $(this).attr("data-state");
+
+          // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+          // Then, set the image's data-state to animate. Else set src to the data-still value
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+      });
+  });
 };
 
 // Call buttonGenerator function to put buttons on UI.
 buttonGenerator();
-
-/* Event listener for all button elements and save info into each data-emotion 
-  when that specific button is clicked. */
-$(".classForButton").on("click", function(e) {
-  var emotion = $(this).attr("data-emotion");
-  var emotionSelected = $(this).text();
-
-  // My API Key:
-  var api_key = "lBtXwR7tqfrf553s7E99F3o2oAa4EC61";
-
-  // URL to be used for get gifs from Giphy with result output limit set to 10.
-  var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    emotionSelected +
-    "&api_key=" +
-    api_key +
-    "&limit=10";
-
-  //Performing the AJAX GET request, then store an array of results in the variable:
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-    // Function to execute when the data gets retrieved from the API:
-    .then(function(response) {
-      var results = response.data;
-      console.log(results);
-
-      // For Loop to be executed for each result item:
-      $("#gifs-display").html("");
-      for (var i = 0; i < results.length; i++) {
-        var image = $("<img>");
-        image.attr("data-still", results[i].images.original_still.url);
-        image.attr("data-animate", results[i].images.original.url);
-        image.attr("data-state", "still");
-        image.css("margin-left", "3px");
-        image.attr("src", results[i].images.original_still.url);
-        image.addClass("gif");
-
-        $("#gifs-display").append(image);
-        var ratingInfo = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
-        $("#gifs-display").append(ratingInfo);         
-      }
-
-      // To animate and pause the result gif image when a user clicks
-      $(".gif").on("click", function() {
-
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-        var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-      });
-    });
-});
 
 // Function to dynamically create a new button with the keyword submitted by the user;
 /* Do this by adding the new word into the emotion array, and then calling the buttonGenerator 
